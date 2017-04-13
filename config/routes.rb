9dc4f2b12a158aa99141936470d208b071039956
JoakimEJ <JoakimEJacobsen@gmail.com>
2017-04-13 22:14:27 +0200

    setting root_url to landingpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'landingpage/landingpage'
-
   resources :projects
+
+  get 'landingpage/landingpage'
+  root to: 'landingpage#landingpage'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
