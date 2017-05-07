49cd0a1d618f820aa12e85f7c47b7bc72cf2797b
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-05-07 13:39:03 +0200

    migrations for cover uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170507101644) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +39,6 @@ ActiveRecord::Schema.define(version: 20170507101644) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
   end
 
 end
