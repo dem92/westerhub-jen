c54dbcbe1515bc3020957745e44cac4ee209ce75
Eva Dahlo <daheva15@student.westerdals.no>
2017-05-11 12:53:05 +0200

    added validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ApplicationRecord
   mount_uploader :cover, CoverUploader
   belongs_to :user
+
+  validates :title, :description, :contact, presence: true
+  validates :title, length: {minimum: 3, maximum: 50}
+  validates :description, length: {minimum: 10}
+  validates :contact, length: {minimum: 8}
 end
