a8578cc2c06aa3bd917c15f315dc19b27d852c46
JoakimEJ <JoakimEJacobsen@gmail.com>
2017-05-12 19:58:22 +0200

    Added profile picture for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511164559) do
+ActiveRecord::Schema.define(version: 20170512150138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 20170511164559) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "cover"
     t.integer  "user_id"
+    t.string   "cover"
     t.string   "contact"
   end
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20170511164559) do
     t.string   "company_name"
     t.text     "about"
     t.text     "skills"
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
