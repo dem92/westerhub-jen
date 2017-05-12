d20c5207717a4c2d8dceaaf0ef07b654615ad396
Eva Dahlo <daheva15@student.westerdals.no>
2017-05-12 11:13:30 +0200

    added project_tag table to db

---
+++ db/migrate/20170511115531_create_project_tags.rb
@@ -0,0 +1,11 @@
+class CreateProjectTags < ActiveRecord::Migration[5.0]
+  def change
+    create_table :project_tags do |t|
+
+      t.integer :project_id
+      t.integer :tag_id
+
+      t.timestamps
+    end
+  end
+end
