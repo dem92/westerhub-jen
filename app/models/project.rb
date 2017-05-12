30ca2e96c891194c18b19574df85d42075494924
Eva Dahlo <daheva15@student.westerdals.no>
2017-05-12 11:19:29 +0200

    updated validation and relationships

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,10 @@
 class Project < ApplicationRecord
   mount_uploader :cover, CoverUploader
   belongs_to :user
+  has_many :project_tags
+  has_many :tags, :through => :project_tags
 
-  validates :title, :description, :contact, presence: true
   validates :title, length: {minimum: 3, maximum: 50}
-  validates :description, length: {minimum: 10}
-  validates :contact, length: {minimum: 8}
+  validates :description, length: {minimum: 10, maximum: 30000}
+  validates :contact, length: {minimum: 8, maximum: 250}
 end
