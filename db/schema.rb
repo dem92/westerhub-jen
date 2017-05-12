d1033e80a83e15c7010d62ac6a8ef3325c4c0b4d
Eva Dahlo <daheva15@student.westerdals.no>
2017-05-12 11:18:07 +0200

    tag and project_tag tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510095919) do
+ActiveRecord::Schema.define(version: 20170511115531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "project_tags", force: :cascade do |t|
+    t.integer  "project_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +30,12 @@ ActiveRecord::Schema.define(version: 20170510095919) do
     t.string   "cover"
     t.integer  "user_id"
     t.string   "contact"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
