dcfb2ed9fd65b117fa920f056b96668a52fe350f
Eva Dahlo <daheva15@student.westerdals.no>
2017-05-12 11:26:08 +0200

    limits shown description if long

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,11 @@
 module ProjectsHelper
+  def limit_description(project)
+    description = project.description
+
+    if description.length > 200
+      return description[0..197] + "..."
+    end
+
+    return description
+  end
 end
