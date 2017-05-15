b09eb06aa3b5bc2c22f48671254085e40c04de0c
JoakimEJ <JoakimEJacobsen@gmail.com>
2017-05-15 19:09:04 +0200

    Regexp validation for first- last- and username + email. Tests for this is currently failing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :username, presence: true
+  validates_format_of :username, with: /[a-z]{3,15}/
   validates :username, uniqueness: true, if: -> { self.username.present? }
+
+  validates_format_of :first_name, with: /\A[A-Z][-a-zA-Z]{1,29}/i
+  validates_format_of :last_name, with: /\A[A-Z][-a-zA-Z]{1,29}/i
+  validates_format_of :email, with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   has_many :projects, dependent: :destroy
 end
