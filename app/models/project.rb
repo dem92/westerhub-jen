2fce5ed592efe11273fe62549fa738e73c5815de
JoakimEJ <JoakimEJacobsen@gmail.com>
2017-05-27 13:30:32 +0200

    created more informative messages on error validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,8 +5,8 @@ class Project < ApplicationRecord
   has_many :project_tags
   has_many :tags, :through => :project_tags
 
-  validates :title, length: {minimum: 3, maximum: 50}
-  validates :description, length: {minimum: 10, maximum: 30000}
-  validates :contact, length: {minimum: 8, maximum: 250}
+  validates :title, length: {minimum: 3, maximum: 50, :message => ": Length must be in range 3-50 characters"}
+  validates :description, length: {minimum: 10, maximum: 30000, :message => ": Length must be in range 10-30000 characters"}
+  validates :contact, length: {minimum: 8, maximum: 250, :message => ": Length must be in range 8-250 characters"}
   validates :user, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :username, presence: true
-  validates_format_of :username, with: /\A[a-z0-9_-]{3,15}\z/
+  validates_format_of :username, with: /\A[a-z0-9_-]{3,15}\z/, :message => ": No uppercase letters and length in range 3-15 characters"
   validates :username, uniqueness: true, if: -> { self.username.present? }
 
-  validates_format_of :first_name, with: /\A[A-Z][-a-zA-Z]{1,29}\z/i
-  validates_format_of :last_name, with: /\A[A-Z][-a-zA-Z]{1,29}\z/i
-  validates_format_of :email, with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates_format_of :first_name, with: /\A[A-Z][-a-zA-Z]{1,29}\z/i, :message => ": Length must be in range 2-30 characters"
+  validates_format_of :last_name, with: /\A[A-Z][-a-zA-Z]{1,29}\z/i, :message => ": Length must be in range 2-30 characters"
+  validates_format_of :email, with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, :message => ": Not a valid email. example: 'name@emailprovider.something'"
 
   has_many :projects, dependent: :destroy
   has_many :comments, dependent: :destroy
